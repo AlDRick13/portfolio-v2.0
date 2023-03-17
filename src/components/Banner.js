70f9b55213763e9948eb5e957672ba6b48641246
AlDRick13 <75345858+AlDRick13@users.noreply.github.com>
2023-03-17 12:56:42 -0600

    feat:animations

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
     return (
         <div >
-            <div className="banner flex ">
-                <div className="m-auto   text-white">
-                    <div className="text-center tracking-primary font-mono font-bold text-4xl m-1 md:m-3 lg-m5 md:text-6xl lg:text-8xl">
-                        Aldo Reygadas
+            <div className="flex banner ">
+                <div className="m-auto text-white">
+                    <div className="m-1 font-mono text-4xl font-bold text-center tracking-primary md:m-3 lg-m5 md:text-6xl lg:text-8xl">
+                        I am Aldo Reygadas
                     </div>
                     <hr className='bg-white' />
-                    <div className="text-center tracking-primary font-mono font-bold text-2xl m-1 md:text-5xl lg:text-7xl">
-                        Full-stack Developer
+                    <div className="m-1 font-mono text-2xl font-bold text-center tracking-primary md:text-5xl lg:text-7xl">
+                        <TypeAnimation
+                            sequence={[
+                                'Full-stack Developer', // Types 'One'
+                                3000, // Waits 1s
+                                'Tech Enthusiast', // Deletes 'One' and types 'Two'
+                                3000, // Waits 2s
+                                'Coder',
+                                3000,
+                                () => {
+                                    console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                                }
+                            ]}
+                            wrapper="div"
+                            cursor={true}
+                            repeat={Infinity}
+                            style={{ fontSize: '2em' }}
+                        />
+
                     </div>
                 </div>
 
             </div>
-            {/* <div className="banner flex sm:hidden">
+            {/* <div className="flex banner sm:hidden">
                 <div className="m-auto text-white">
                     <Typography
 
